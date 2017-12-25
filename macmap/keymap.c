e835f5e1c87f311af9c1d68fa979008ef889523b
Carlos Filoteo <c.filoteo@live.com>
2017-12-24 21:44:27 -0700

    Fix typos

--- macmap/keymap.c
+++ macmap/keymap.c
@@ -1,4 +1,5 @@
 #include "gherkin.h"
+#include "action_layer.h"
 
 //Friendly Layer Names
 enum gherkin_layers {
@@ -19,17 +20,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = KEYMAP(
     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_ENT,  
-    KC_Z,     KC_X,     KC_C,     KC_V,     KC_LGUI,  KC_SPC,   KC_B,     KC_N,     KC_M,     UPPR,
+    KC_Z,     KC_X,     KC_C,     KC_V,     KC_LGUI,  KC_SPC,   KC_B,     KC_N,     KC_M,     UPPR
   ),
   [_UPPERS] = KEYMAP(
-    KC_ESC,   KC_GRV,   KC_MINS,  KC_EQLS,  KC_LBRC,  KC_RBRC,  KC_1,     KC_2,     KC_3,     KC_BSPC,
+    KC_ESC,   KC_GRV,   KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_1,     KC_2,     KC_3,     KC_BSPC,
     KC_TAB,   KC_BSLS,  KC_SCLN,  KC_QUOT,  KC_LEFT,  KC_RGHT,  KC_4,     KC_5,     KC_6,     KC_0,
-    KC_CAPS,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP  ,  KC_DOWN,  KC_7,     KC_8,     KC_9,     FNRW,
+    KC_CAPS,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP  ,  KC_DOWN,  KC_7,     KC_8,     KC_9,     FNRW
   ),
   [_FNROW] = KEYMAP(
     KC_F1,    KC_F2,    KC_F3,    KC_F4,    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_VOLD,  KC_VOLU,  KC_MUTE,
     KC_F5,    KC_F6,    KC_F7,    KC_F8,    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_MFFD,  KC_MPLY,  KC_MRWD,
-    KC_F9,    KC_10,    KC_F11,   KC_F12,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  RESET,    BASE,
+    KC_F9,    KC_F10,   KC_F11,   KC_F12,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  RESET,    BASE
   ),
 };
 
