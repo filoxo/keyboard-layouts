a9f6e2e335a1591e420aa4b801cbd9dc4a17800b
Carlos Filoteo <c.filoteo@live.com>
2017-12-26 18:09:47 -0700

    Fix number pad layout

--- macmap/keymap.c
+++ macmap/keymap.c
@@ -32,9 +32,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Z,        KC_X,     KC_C,     KC_V,     CMD_BSP,  SFT_SPC,  KC_B,     KC_N,     KC_M,     UPPR
   ),
   [_UPPERS] = KEYMAP(
-    KC_ESC,   KC_GRV,   KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_1,     KC_2,     KC_3,     KC_BSPC,
+    KC_ESC,   KC_GRV,   KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_7,     KC_8,     KC_9,     KC_BSPC,
     KC_TAB,   KC_BSLS,  KC_SCLN,  KC_QUOT,  KC_LEFT,  KC_RGHT,  KC_4,     KC_5,     KC_6,     KC_0,
-    KC_CAPS,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP  ,  KC_DOWN,  KC_7,     KC_8,     KC_9,     FNRW
+    KC_CAPS,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP  ,  KC_DOWN,  KC_1,     KC_2,     KC_3,     FNRW
   ),
   [_FNROW] = KEYMAP(
     KC_F1,    KC_F2,    KC_F3,    KC_F4,    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_VOLD,  KC_VOLU,  KC_MUTE,
