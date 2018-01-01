192c6e0792796dae3ee3b58c48bd44e51b9e1fd2
Carlos Filoteo <c.filoteo@live.com>
2017-12-31 20:35:38 -0700

    Modify base layer

--- macmap/keymap.c
+++ macmap/keymap.c
@@ -19,8 +19,7 @@ enum {
 #define BASE TO(_QWERTY)
 #define UPPR TO(_UPPERS)
 #define FNRW TO(_FNROW)
-#define SPC_SFT SFT_T(KC_SPC)
-#define BSP_CMD GUI_T(KC_BSPC)
+#define SPC_GUI GUI_T(KC_SPC)
 #define UPPR_ENT LT(UPPR, KC_ENT)
 
 // Fillers to make layering more clear
@@ -31,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = KEYMAP(
     TD(Q_ESC),   KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     TD(P_BSP),
     TD(A_TAB),   KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     UPPR_ENT,
-    KC_Z,        KC_X,     KC_C,     KC_V,     KC_B,     SPC_SFT,  KC_N,     KC_M,     TD(DT_CM),BSP_CMD
+    KC_Z,        KC_X,     KC_C,     KC_V,     KC_B,     SPC_GUI,  KC_N,     KC_M,     TD(DT_CM),KC_BSPC
   ),
   [_UPPERS] = KEYMAP(
     KC_ESC,   KC_GRV,   KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_7,     KC_8,     KC_9,     KC_BSPC,
