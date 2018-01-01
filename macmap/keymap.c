008d9b1f6e1a4a46db949d3416e82436a2fda13f
Carlos Filoteo <c.filoteo@live.com>
2017-12-31 21:28:13 -0700

    Fix spacing for better readability

--- macmap/keymap.c
+++ macmap/keymap.c
@@ -32,19 +32,19 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = KEYMAP(
-    TD(Q_ESC),   KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     TD(P_BSP),
-    TD(A_TAB),   KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     ENT_UPPR,
-    KC_Z,        KC_X,     KC_C,     KC_V,     KC_B,     SPC_GUI,  KC_N,     KC_M,     TD(DT_CM),KC_BSPC
+    TD(Q_ESC), KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,     KC_U,      KC_I,      KC_O,      TD(P_BSP),
+    TD(A_TAB), KC_S,      KC_D,      KC_F,      KC_G,      KC_H,     KC_J,      KC_K,      KC_L,      ENT_UPPR,
+    KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      SPC_GUI,  KC_N,      KC_M,      TD(DT_CM), KC_BSPC
   ),
   [_UPPERS] = KEYMAP(
-    KC_1,       KC_2,      KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
-    XXXXXXX,    KC_UP,     XXXXXXX,  XXXXXXX,  KC_GRV,   KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  BSL_FNRW,
-    KC_LEFT,    KC_DOWN,   KC_RGHT,  XXXXXXX,  XXXXXXX,  KC_SCLN,  KC_QUOT,  KC_COMM,  KC_DOT,   KC_SLSH
+    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,     KC_8,      KC_9,      KC_0,
+    XXXXXXX,   KC_UP,     XXXXXXX,   XXXXXXX,   KC_GRV,    KC_MINS,   KC_EQL,   KC_LBRC,   KC_RBRC,   BSL_FNRW,
+    KC_LEFT,   KC_DOWN,   KC_RGHT,   XXXXXXX,   XXXXXXX,   KC_SCLN,   KC_QUOT,  KC_COMM,   KC_DOT,    KC_SLSH
   ),
   [_FNROW] = KEYMAP(
-    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,
-    KC_F11,   KC_F12,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_MFFD,  KC_MPLY,  MRWD_BASE,
-    BRT_DN,   BRT_UP,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_VOLD,  KC_VOLU,  KC_MUTE
+    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,    KC_F8,     KC_F9,     KC_F10,
+    KC_F11,    KC_F12,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  KC_MFFD,   KC_MPLY,   MRWD_BASE,
+    BRT_DN,    BRT_UP,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  KC_VOLD,   KC_VOLU,   KC_MUTE
   ),
 };
 
