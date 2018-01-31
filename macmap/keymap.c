ddbebd068a85bb17cf87946fff8adfcae814c756
Carlos Filoteo <c.filoteo@live.com>
2018-01-31 11:57:36 -0700

    Add arrows

--- macmap/keymap.c
+++ macmap/keymap.c
@@ -46,9 +46,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
       TRNS , TRNS  , TRNS  , TRNS  , TRNS  , TRNS  ,                    TRNS ,  TRNS ,  TRNS ,  LCBR ,  RCBR ,  TRNS ,
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-      TRNS , TRNS  , TRNS  , TRNS  , TRNS  , TRNS  ,                    TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,
+      TRNS , TRNS  , TRNS  , TRNS  , TRNS  , TRNS  ,                    TRNS ,   UP  ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,
   //|------|-------|-------|-------|-------|-------|-------   -------|-------|-------|-------|-------|-------|-------|
-      TRNS , TRNS  , TRNS  , TRNS  , TRNS  , TRNS  , TRNS ,     TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,
+      TRNS , TRNS  , TRNS  , TRNS  , TRNS  , TRNS  , TRNS ,     TRNS ,  LEFT ,  DOWN , RIGHT ,  TRNS ,  TRNS ,  TRNS ,
   //|------|-------|-------|-------|-------|-------|------/   \------|-------|-------|-------|-------|-------|-------|
                                      TRNS  , TRNS  , DEL  ,     TRNS ,  TRNS ,  TRNS 
   //                               .-------|-------/-----/     \-----|-------|-------.
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
       TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,                    TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,
   //|------|-------|-------|-------|-------|-------|------/   \------|-------|-------|-------|-------|-------|-------|
-      TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS , TRNS ,     TRNS ,  TRNS , TRNS  ,  TRNS ,  TRNS ,  TRNS ,  TRNS , 
+      TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS , TRNS ,     TRNS ,  TRNS ,  TRNS  , TRNS ,  TRNS ,  TRNS ,  TRNS , 
   //|------|-------|-------|-------|-------|-------|------/   \------|-------|-------|-------|-------|-------|-------|
                                       TRNS ,  TRNS , TRNS ,     TRNS ,  TRNS , TRNS
   //                               .-------|-------/-----/     \-----|-------|-------.
