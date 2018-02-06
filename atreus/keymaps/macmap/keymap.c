a1588073ea52d8e56b5931fea5ee5728aea3f079
Carlos Filoteo <c.filoteo@live.com>
2018-02-06 00:48:13 -0700

    Fix bad KC_ prefixes

--- atreus/keymaps/macmap/keymap.c
+++ atreus/keymaps/macmap/keymap.c
@@ -22,9 +22,9 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = KEYMAP(
-   Q   ,   W   ,   E   ,   R   ,   T   ,                   Y   ,   U   ,   I   ,   O   ,   P   ,
-   A   ,   S   ,   D   ,   F   ,   G   ,                   H   ,   J   ,   K   ,   L   , KC_ENT,
-   Z   ,   X   ,   C   ,   V   ,   B   ,                   N   ,   M   ,KC_COMM, KC_DOT,KC_SLSH,
+KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,
+KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_ENT ,
+KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,                KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,
  KC_ESC, KC_TAB,KC_LGUI,KC_LCTL, RAISE ,KC_BSPC, KC_SPC, LOWER ,KC_LSFT,KC_LALT,KC_SCLN,KC_QUOT
 ),
 
@@ -44,7 +44,7 @@ _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
 
 [_ADJUST] = KEYMAP(
  RESET ,_______,_______,_______,_______,                _______,KC_MUTE,KC_VOLD,KC_VOLU,_______,
-_______,_______,  F14  ,  F15  ,_______,                _______,KC_MPRV,KC_MPLY,KC_MNXT,_______,
+_______,_______,KC_F14 ,KC_F15 ,_______,                _______,KC_MPRV,KC_MPLY,KC_MNXT,_______,
 _______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,
 _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______
 )
